98e798c5a4f501d584d853fe92269a16eb86fb53
LaWox <woxler.platon@gmail.com>
2020-05-23 17:04:33 +0200

    small change

--- clusters.py
+++ clusters.py
@@ -1,6 +1,13 @@
 import numpy as np
 
 def getN(set1, set2):
+    ''' output the number matrix n given to sets of clusters
+    Parameter:
+        set1: List of list --> [[observations in cluster 1], [.... in cluster 2], [....]]
+        set2: same as list 2
+    Returns:
+        n: np matrix, number matrix
+    '''
     n = np.zeros((len(set1), len(set2))) 
     for i in range(len(set1)):
         for elem in set1[i]: 
@@ -26,10 +33,10 @@ def kMeans(observations, centers):
     distance = np.zeros(len(centers))
     means = np.zeros((len(centers), obsShape))
     newMeans = np.zeros((len(centers), obsShape))
-    unchanged = True
+    changed = True
 
-    # init empty clusters
-    while unchanged:
+    while changed:
+        # init empty clusters
         for center in centers:
             cluster = []
             clusters.append(cluster)
@@ -50,10 +57,13 @@ def kMeans(observations, centers):
         
         # stopping condition
         for i in range(means.shape[0]):
-            if (means[i] != newMeans[i]).all():
-                unchanged = False
+            if (means[i] != newMeans[i]).all(): # if any of them are changed keep going
                 break
 
+        # nothing changed cluster, end loop
+        changed = False
+
+    # print final results
     print(centers)
     for cluster in clusters:
         print(cluster)
